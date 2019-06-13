814ba085cfb94c7107bdb72071b1cae5ac4aa1eb
Cristian Alberch <cristian.alberch@outlook.com>
2019-06-13 21:07:25 +0300

    jupyter notebook code into process_data

--- data/process_data.py
+++ data/process_data.py
@@ -1,18 +1,56 @@
 import sys
 
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath) #load csv file into pd
+    categories = pd.read_csv(categories_filepath) #load csv files into pd
+    df = pd.merge(messages, categories, on='id') #merge 'messages' and 'categories' pd files
+    return df
 
 def clean_data(df):
-    pass
+
+    categories =  df['categories'].str.split(";",expand=True)
+  
+    #select the first row of the categories dataframe to extract column names 
+    category_colnames = categories.iloc[0].str.slice(stop=-2)
+
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.slice(start=-1)
+    
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+    
+    df = df.drop('categories', axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+
+    #arrange columns in alphabetical order
+    cols = ['id','message','original','genre','aid_centers', 'aid_related', 'buildings', 'child_alone', 'clothing',
+        'cold', 'death', 'direct_report', 'earthquake', 'electricity', 'fire',
+        'floods', 'food',  'hospitals',  'infrastructure_related',
+        'medical_help', 'medical_products',  'military',
+        'missing_people', 'money', 'offer',  'other_aid',
+        'other_infrastructure', 'other_weather', 'refugees', 'related',
+        'request', 'search_and_rescue', 'security', 'shelter', 'shops', 'storm',
+        'tools', 'transport', 'water', 'weather_related']
+    df = df[cols]
+
+    #drop duplicate columns
+    df.drop_duplicates(subset ="id", keep = False, inplace = True)
 
 
 def save_data(df, database_filename):
-    pass  
 
+    engine = create_engine('sqlite:///DisasterResponseDataBase.db')
+    df.to_sql('MessageClassification', engine, index=False)
 
+  
 def main():
     if len(sys.argv) == 4:
 
